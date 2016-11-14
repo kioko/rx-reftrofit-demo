d324d414bbb02b7765a3ff2c00a8f97d15c84133
Thomas Kioko <kiokotomas@gmail.com>
2016-11-14 09:17:01 +0300

    Create interface class

---
+++ app/src/main/java/com/thomaskioko/rxretrofit/service/MovieServices.java
@@ -0,0 +1,37 @@
+package com.thomaskioko.rxretrofit.service;
+
+
+import com.thomaskioko.rxretrofit.model.MovieResult;
+
+import retrofit2.http.GET;
+import rx.Observable;
+
+
+/**
+ * This class contains API functions
+ *
+ * @author Kioko
+ * @version Version
+ */
+
+
+public interface MovieServices {
+
+    /**
+     * Get top rated movies
+     *
+     * @return JSON Result
+     */
+    @GET("top_rated?")
+    Observable<MovieResult> getTopRatedMovies();
+
+    /**
+     * Get popular movies.
+     *
+     * @return JSON Result
+     */
+    @GET("popular?")
+    Observable<MovieResult> getPopularMovies();
+
+}
+
