8b7e696f8fd3db178c414b56c2a60536cb77a52c
Thomas Kioko <kiokotomas@gmail.com>
2016-12-05 21:26:52 +0300

    Add image dimension sizes.
    The dimensions are required when making API requests to TMDB

--- app/src/main/java/com/thomaskioko/rxretrofit/util/ApplicationConstants.java
+++ app/src/main/java/com/thomaskioko/rxretrofit/util/ApplicationConstants.java
@@ -18,6 +18,26 @@ public class ApplicationConstants {
      */
     public static final String END_POINT = "http://api.themoviedb.org/3/movie/";
     /**
+     * Image base Url
+     */
+    public static final String TMDB_IMAGE_URL = "http://image.tmdb.org/t/p/";
+    /**
+     * 185 Image size
+     */
+    public static final String IMAGE_SIZE_185 = "w185";
+    /**
+     * 500 Image size
+     */
+    public static final String IMAGE_SIZE_500 = "w500";
+    /**
+     * 780 Image size
+     */
+    public static final String IMAGE_SIZE_780 = "w780";
+    /**
+     * Original Image size
+     */
+    public static final String IMAGE_SIZE_ORIGINAL = "original";
+    /**
      * Connection timeout duration
      */
     public static final int CONNECT_TIMEOUT = 60 * 1000;
