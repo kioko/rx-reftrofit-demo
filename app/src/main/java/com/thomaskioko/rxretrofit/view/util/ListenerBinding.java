f619bd368c9312ed20688b1a5ce5bcefd9b2c91a
Thomas Kioko <kiokotomas@gmail.com>
2017-01-10 16:49:07 +0300

    Create listener class
    handles onClickEvents in the app. This helps us reuse this class

--- app/src/main/java/com/thomaskioko/rxretrofit/view/util/ListenerBinding.java
+++ app/src/main/java/com/thomaskioko/rxretrofit/view/util/ListenerBinding.java
@@ -1,8 +1,36 @@
 package com.thomaskioko.rxretrofit.view.util;
 
+import android.view.View;
+
+import com.thomaskioko.rxretrofit.R;
+import com.thomaskioko.rxretrofit.databinding.ListMovieItemBinding;
+import com.thomaskioko.rxretrofit.util.DisplayUtils;
+
 /**
+ * Binding class to handle on click events.
+ *
  * @author Thomas Kioko
  */
 
-public class ListenerBinding {
+public class ListenerBinding implements View.OnClickListener {
+
+    private ListMovieItemBinding mListMovieItemBinding;
+
+    /**
+     * Constructor
+     *
+     * @param listMovieItemBinding Binding item
+     */
+    public ListenerBinding(ListMovieItemBinding listMovieItemBinding) {
+        mListMovieItemBinding = listMovieItemBinding;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.movie_image:
+                DisplayUtils.showToastMessage(view.getContext(), mListMovieItemBinding.getMovie().getTitle(), true);
+                break;
+        }
+    }
 }
