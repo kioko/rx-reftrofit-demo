dbc888940ea345dd3b56b2a54c931cfebe9a3106
Thomas Kioko <kiokotomas@gmail.com>
2017-01-10 17:13:33 +0300

    Clean class
    - Remove on click functionality. This is being handled by the Listener class.

--- app/src/main/java/com/thomaskioko/rxretrofit/view/adapter/MovieListAdapter.java
+++ app/src/main/java/com/thomaskioko/rxretrofit/view/adapter/MovieListAdapter.java
@@ -7,10 +7,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.thomaskioko.rxretrofit.BR;
 import com.thomaskioko.rxretrofit.R;
 import com.thomaskioko.rxretrofit.databinding.ListMovieItemBinding;
 import com.thomaskioko.rxretrofit.model.Movie;
-import com.thomaskioko.rxretrofit.util.DisplayUtils;
+import com.thomaskioko.rxretrofit.view.util.ListenerBinding;
 
 import java.util.List;
 
@@ -40,14 +41,17 @@ public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.View
         View view = inflater.inflate(R.layout.list_movie_item, parent, false);
         ListMovieItemBinding binding = ListMovieItemBinding.bind(view);
 
+        //Set listener
+        binding.setListeners(new ListenerBinding(binding));
+
         return new ViewHolder(binding.getRoot());
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         Movie movie = mMovieList.get(position);
-        ListMovieItemBinding binding = DataBindingUtil.getBinding(holder.itemView);
-        holder.bindTo(binding, movie);
+        ViewDataBinding binding = holder.getBinding();
+        binding.setVariable(BR.movie, movie); //Bind the variable
     }
 
     @Override
@@ -59,37 +63,19 @@ public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.View
      * Class that helps us implement {@link android.support.v7.widget.RecyclerView.ViewHolder} pattern
      * in the adapter.
      */
-    static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        private Movie movie;
-        private ViewDataBinding mViewDataBinding;
+    static class ViewHolder extends RecyclerView.ViewHolder {
 
         ViewHolder(View itemView) {
             super(itemView);
-            mViewDataBinding = DataBindingUtil.bind(itemView);
-            itemView.setTag(mViewDataBinding);
         }
 
         /**
-         * Helper method used to bind objects to the view.
+         * Method that returns Binding instance.
          *
-         * @param binding {@link ListMovieItemBinding}
-         * @param movie   {@link Movie}
+         * @return {@link ListMovieItemBinding}
          */
-        void bindTo(ListMovieItemBinding binding, Movie movie) {
-
-            this.movie = movie;
-            binding.setMovie(movie);
-            binding.movieImage.setOnClickListener(this);
-            binding.executePendingBindings();
-        }
-
-        @Override
-        public void onClick(View view) {
-            switch (view.getId()) {
-                case R.id.movie_image:
-                    DisplayUtils.showToastMessage(view.getContext(), movie.getTitle(), true);
-                    break;
-            }
+        ViewDataBinding getBinding() {
+            return DataBindingUtil.getBinding(itemView);
         }
     }
 
